ff7bf33c9b297f901f4653ceb38672ed658b469f
Jun Honda <honjetman@gmail.com>
2020-01-19 16:58:04 +0900

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const publidDir = __dirname + '/public';
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: publidDir,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: publidDir
+  }
+};
