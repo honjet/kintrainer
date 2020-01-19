f4afe9cd4a9b2f2a3c6ce3888dd47c14646ac59a
Jun Honda <honjetman@gmail.com>
2020-01-19 17:42:12 +0900

    add prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div> Hello World </div>, document.getElementById("root"));
+ReactDOM.render(<div> Hello World </div>, document.getElementById('root'));
