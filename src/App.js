41dc0d42040a0b7b1d881b871c6c01dd7f342ce1
son <m5161151@outlook.jp>
2020-01-31 00:56:01 +0900

    ヘッダ追加

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Do from '@/pages/do/Do';
 import DoContext, {workoutContext} from '@/contexts/doContext';
+import Header from './Header';
 
 function App(props) {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <DoContext.Provider value={workoutContext}>
           <Route exact path="/" component={Do} />
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Header = () => (
+  <header><h1>筋とけ</h1></header>
+);
+
+export default Header;
