bfad307222212e41f3b005b3d9a1af70ec4a6d6b
son <m5161151@outlook.jp>
2020-02-04 00:00:33 +0900

    ヘッダーをいい感じにしました。

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <header><h1>筋とけ</h1></header>
+  <header>
+    <h1>筋トレーナー</h1>
+    <p>~君だけの筋肉を育てよう~</p>
+  </header>
+
 );
 
 export default Header;
